b3de66eeee0165c498de0e78be80b2262a99edb2
JulioDiazT <116096551+JulioDiazT@users.noreply.github.com>
2022-10-20 13:21:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const Lista =[];
+  
+   const mostrar = ()=>{ 
+    var x=1;
+ 
+    for(x=1;x<101;x++){
+        if(x%5==0 && x % 3 == 0){
+          Lista.push('Fizz Buzz', )
+        }else if(x%5==0 ){
+          Lista.push("Buzz" , )
+        } else if ( x % 3 == 0){
+          Lista.push( "Fizz", )
+        }else {
+          Lista.push(x , )
+        }
+    }
+      document.getElementById("lista").textContent=Lista
+    }
+
+    const borrar = ()=>{ 
+    
+      Lista.splice(0, Lista.length);
+      document.getElementById("lista").textContent=Lista;
+      }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3> EJERCICIO </h3>
+        <button onClick={mostrar}>  EJECUTAR </button>
+        <button onClick={borrar}>  BORRAR </button>
+        <p id="lista" >  </p>
       </header>
+    
     </div>
+   
   );
 }
 
